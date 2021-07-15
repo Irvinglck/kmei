e8b4b302585c0c3de32c6ee9177c3db7d27fb32e
Irvinglck <954386549@qq.com>
2021-07-15 21:47:49 +0800

    jvm 学习

--- reverse/src/main/java/com/lck/reverse/stu/jvm/JvmDemo.java
+++ reverse/src/main/java/com/lck/reverse/stu/jvm/JvmDemo.java
@@ -1,4 +1,26 @@
 package com.lck.reverse.stu.jvm;
 
+import com.lck.reverse.commons.Person;
+
 public class JvmDemo {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        //获取person的实例模板
+        Class<? extends Person> aClass = person.getClass();
+        //通过模板可以获取类加载器
+        ClassLoader classLoader = aClass.getClassLoader();
+        //appClassLoader加载器加载的类
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(classLoader.getParent().toString());
+
+//        Thread thread = new Thread();
+//        thread.start();
+        //返回虚拟机视图使用最大内存
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        System.out.println("MAX_MEMORY = " + maxMemory + "（字节）、" + (maxMemory / (double) 1024 / 1024));
+        // 返回 Java 虚拟机中的内存总量
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println("TOTAL_MEMORY = " + totalMemory + "（字节）、" + (totalMemory / (double) 1024 / 1024) + "MB");
+    }
 }
